c531cc9fb0031abd5c9932ed257ddd61ecc93d12
Deysi <sude.90@gmail.com>
2017-10-16 12:12:44 -0500

    primera version home

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {
+	BrowserRouter,
+	Route,
+	Switch,
+	NavLink,
+	Redirect
+} from 'react-router-dom'
+import './App.css';
+
+import Home from './Home';
+import SignUp from './SignUp';
+
+
+const NotFound = (props) => {
+	return (
+		<div><h2> Error 404! </h2></div>
+	);
+}
+
+const App = (props) => {
+	const {model} =  props;
+	console.log('app.props', props)
+	return (<BrowserRouter>
+		<div>
+			<Switch>
+				<Route exact path="/"
+				       render={() => <Redirect to= {'/home'}/>}/>
+				<Route path="/Lyft-Evaluado"
+				       render={() => <Redirect to= {'/home'}/>}/>
+				<Route  path="/home" render={() => <Home model={model} />}/>
+				<Route  path="/signup" render={() => <SignUp model={model} />}/>
+			
+				<Route component={NotFound}/>
+			</Switch>
+		</div>
+	</BrowserRouter>)
+}
+
+export default App;
