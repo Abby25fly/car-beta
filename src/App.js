d20eb7f3db9d49acc9844c3c4c21c4ccd0c0819a
Abigail <abi25.fly@gmail.com>
2017-10-16 12:07:35 -0500

    SignUp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Parts from './Parts';
+import SignUpCar from './SignUpCar';
+import SignUpUser from './SignUpUser';
 
 class App extends Component {
   render() {
     return (
-      <Parts/>
+      <SignUpUser/>
     );
   }
 }
