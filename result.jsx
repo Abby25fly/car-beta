929330479cc8429e561f5f1cf98dd61c5c546060
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-16 18:20:35 -0500

    Dos componentes finalizados

--- result.jsx
+++ result.jsx
@@ -1,34 +1,50 @@
-
 import React, { Component } from "react";
 import './Result.css';
-const Result = () => (
+
+const DataProcessEnough = () => (
   <div className="container">
-  <Col xs={6} md={4} id="card">
-  <Thumbnail src="https://rightindem.com/wp-content/themes/HTML5-Reset-WordPress-Theme-master/images/icon-insurer.png" alt="242x200">
-    <h3 id="name">Name's client</h3>
-    <p id="info-card">Description</p>
-    <p>
-      <Button bsStyle="primary">Atrás</Button>&nbsp;
-      <Button bsStyle="default">Adelante</Button>
-    </p>
-  </Thumbnail>
-</Col>
-</div>
-  );
-  
+    <Col xs={6} md={4} id="card">
+      <Thumbnail src="public/avatar.png" alt="242x200">
+        <h3 id="name">Name's client</h3>
+        <p id="info-card">Description</p>
+      </Thumbnail>
+      <div>
+        <Button id="btnNext"><i><Glyphicon glyph="earphone" /></i></Button>
+      </div>
+    </Col>
+  </div>);
 
 
-const BtnCall = ()=>(
-    <div className="well" id="btnNext">
-    
+const DataProcessCorrect = () => (
+  <div className="container">
+    <Col xs={6} md={4} id="card">
+      <Thumbnail src="https://rightindem.com/wp-content/themes/HTML5-Reset-WordPress-Theme-master/images/icon-claims-manager.png" alt="242x200">
+        <h3 id="name">Name's client</h3>
+        <p id="info-card">Description</p>
+
+        <div className="well">
+
+          <Button id="btnNext" bsSize="large" block>Confirmar</Button>
+        </div>
+
+      </Thumbnail>
+    </Col>
+  </div>
+);
+
+
+
+const BtnCall = () => (
+  <div className="well" id="btnNext">
+
     <Button bsSize="large" block>Llamar</Button>
   </div>
 
 );
 
-const BtnNext = ()=>(
-    <div className="well" id="btnNext">
-    
+const BtnNext = () => (
+  <div className="well" id="btnNext">
+
     <Button bsSize="large" block>Siguiente</Button>
   </div>
 
