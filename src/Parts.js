319344cfceb13e7fb36bd1e22098c209da5108c9
solics <s.huayhua.ma@gmail.com>
2017-10-16 11:14:33 -0500

    uploading

--- src/Parts.js
+++ src/Parts.js
@@ -9,17 +9,21 @@ class Parts extends Component {
       fwl:'',
       fwr:'',
       bonnet:'',
+      ws:'',
       fdl:'',
       fdr:'',
       roof:'',
       bdl:'',
       bdr:'',
       rw:'',
-      bumper:''
+      bumper:'',
+      tl:'',
+      tr:''
     };
   }
   select(e){
-    
+    e.sele
+
   }
   render() {
     return (
@@ -30,39 +34,39 @@ class Parts extends Component {
         <div className='row'>
           <div className='s-car'>
             <div className='one col-md-12'>
-              <div className='box' onClick={this.select}>Front</div>
+              <div className={'box'+this.state.fr} onClick={this.select}>Frente</div>
             </div>
             <div className='two col-md-12'>
-              <div className='box' onClick={this.select}>Front Wing Left</div>
-              <div className='box' onClick={this.select}>Front Wing Right</div>
+              <div className={'box'+this.state.fwl} onClick={this.select}>Ala Izquierda</div>
+              <div className={'box'+this.state.fwr} onClick={this.select}>Ala Derecha</div>
             </div>
             <div className='one col-md-12'>
-              <div className='box' onClick={this.select}>Bonnet</div>
+              <div className={'box'+this.state.bonnet} onClick={this.select}>Capot</div>
             </div>
             <br/>
             <div className='one col-md-12'>
-              <div className='box' onClick={this.select}>Windscreen</div>
+              <div className={'box'+this.state.ws} onClick={this.select}>Parabrisas</div>
             </div>
             <br/>
             <div className='two col-md-12'>
-              <div className='box' onClick={this.select}>Front Door Left</div>
-              <div className='box' onClick={this.select}>Front Door Right</div>
+              <div className={'box'+this.state.fdl} onClick={this.select}>Puerta Delantera Izquierda</div>
+              <div className={'box'+this.state.fdr} onClick={this.select}>Puerta Delantera Derecha</div>
             </div>
             <div className='one col-md-12'>
-              <div className='box' onClick={this.select}>Roof</div>
+              <div className={'box'+this.state.roof} onClick={this.select}>Techo</div>
             </div>
             <div className='two col-md-12'>
-              <div className='box' onClick={this.select}>Back Door Left</div>
-              <div className='box' onClick={this.select}>Back Door Right</div>
+              <div className={'box'+this.state.bdl} onClick={this.select}>Puerta Trasera Izquierda</div>
+              <div className={'box'+this.state.bdr} onClick={this.select}>Puerta Trasera Derecha</div>
             </div>
-            <br/><br/>
+            <br/>
             <div className='one col-md-12'>
-              <div className='box' onClick={this.select}>Rear Window</div>
+              <div className={'box'+this.state.rw} onClick={this.select}>Ventana Trasera</div>
             </div>
             <div className='two col-md-12'>
-              <div className='box' onClick={this.select}>Tail Left</div>
-              <div className='box' onClick={this.select}>Bumper</div>
-              <div className='box' onClick={this.select}>Tail Right</div>
+              <div className={'box'+this.state.tl} onClick={this.select}>Cola Izquierda</div>
+              <div className={'box'+this.state.bumper} onClick={this.select}>Maletero</div>
+              <div className={'box'+this.state.tr} onClick={this.select}>Cola Derecha</div>
             </div>
           </div>
         </div>
