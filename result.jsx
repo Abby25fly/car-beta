bac11d16088fd201f712b9cc7e34db0464abe5b7
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-17 10:34:05 -0500

    class

--- result.jsx
+++ result.jsx
@@ -1,6 +1,37 @@
 import React, { Component } from "react";
+import './Data.jsx';
 import './Result.css';
 
+class FinalData extends Component {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        completed: false
+      }
+  
+      this.userInfo = {
+        phone: null
+      }
+    }
+  
+  
+    Solutions.map(item => {
+      return (<div className="container">
+      <Col xs={6} md={4} id="card">
+        <Thumbnail src="../public/avatar.png" alt="242x200">
+          <h3 id="name">{extraer input del nombre del usuario}</h3>
+          <p id="info-card">{item.mechanicService}</p>
+        </Thumbnail>
+        <div>
+          <Button id="btnNext"><i><Glyphicon glyph="earphone" /></i></Button>
+        </div>
+      </Col>
+    </div>);
+      
+  }
+
+  
 const DataProcessEnough = () => (
   <div className="container">
     <Col xs={6} md={4} id="card">
@@ -49,18 +80,11 @@ const BtnNext = () => (
 
 );
 
-Solutions.map(item => {
-  return (<div className="container">
-  <Col xs={6} md={4} id="card">
-    <Thumbnail src="../public/avatar.png" alt="242x200">
-      <h3 id="name">{extraer input del nombre del usuario}</h3>
-      <p id="info-card">{item.mechanicService}</p>
-    </Thumbnail>
-    <div>
-      <Button id="btnNext"><i><Glyphicon glyph="earphone" /></i></Button>
-    </div>
-  </Col>
-</div>);
+}  
+export default FinalData;
+
+
+
 
     
 
