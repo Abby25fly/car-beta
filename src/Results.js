ecfe141bda87c7044636eda54be3702bcb6f5531
jais210 <jakeline_210@hotmail.com>
2017-10-17 17:13:20 -0500

    finalizado

--- src/Results.js
+++ src/Results.js
@@ -54,46 +54,5 @@ class Results extends Component {
                 </Col>
               </div>)
       }  
-// const DataProcessEnough = () => (
-//   <div className="container">
-//     <Col xs={6} md={4} id="card">
-//       <Thumbnail src="../public/avatar.png" alt="242x200">
-//         <h3 id="name">Name's client</h3>
-//         <p id="info-card">Description</p>
-//       </Thumbnail>
-//       <div>
-//         <Button id="btnNext"><i><Glyphicon glyph="earphone" /></i></Button>
-//       </div>
-//     </Col>
-//   </div>);
-
-// const DataProcessCorrect = () => (
-//   <div className="container">
-//     <Col xs={6} md={4} id="card">
-//       <Thumbnail src="../public/avatar.png">
-//         <h3 id="name">Name's client</h3>
-//         <p id="info-card">Description</p>
-
-//         <div className="well">
-
-//           <Button id="btnNext" bsSize="large" block>Confirmar</Button>
-//         </div>
-
-//       </Thumbnail>
-//     </Col>
-//   </div>
-// );
-
-
-
-
-// const BtnNext = () => (
-//   <div className="well" id="btnNext">
-
-//     <Button bsSize="large" block>Siguiente</Button>
-//   </div>
-
-// );
-
 }  
 export default Results;
--- src/datainfo.js
+++ src/datainfo.js
@@ -4,42 +4,42 @@ const DataInfo ={
     {
         nameDamage:"Desbarranco",
         damage:"Daño total del vehículo",
-        description:"Su seguro le devuelve un nuevo vehículo con las características dadas al momento de la adquisición del seguro.",
+        description:"Le devuelve un nuevo vehículo con las características dadas al momento de la adquisición del seguro.",
         solutions:[0,1,3,4,5],
         id:1
     },
     {
         nameDamage: "Avería Mecánica",
         damage:"Falla mecánica",
-        description: "Su seguro le garantiza la reparación total del auto, enviandole un mecánico a su ubicación y/o grúa de ser necesario.",
+        description: "Le garantiza la reparación total de su vehículo, enviandole un mecánico a su ubicación y/o grúa de ser necesario.",
         solutions:[0,1,3],
         id: 2
     },
     {
         nameDamage: "Choque provocado por el conductor",
         damage:"Daño Propio",
-        description: "Cubre los daños o pérdidas que sufra el vehículo asegurado por las maniobras hechas por el conductor: rotura de lunas y accesorios musicales.",
+        description: "Cubre los daños o pérdidas que sufra su vehículo: rotura de lunas y accesorios musicales.",
         solutions:[0,1,3,5],
         id: 3
     },
     {
         nameDamage: "Choque Frontal",
         damage:"Accidente de los ocupantes",
-        description: "Su seguro responderá por las coberturas de gastos de curación de los ocupantes del vehículo asegurado y de los daños ocasionados al vehiculo.",
+        description: "Responderá por las coberturas de gastos de curación de los ocupantes de su vehículo y de los daños ocasionados.",        
         solutions:[0,4,5],
         id: 4
     },
     {
         nameDamage: "Volcadura",
         damage:"Daño por despite o imperfecto mecánico",
-        description: "Su seguro responderá por las coberturas de muerte, invalidez permanente total o parcial y gastos de curación de los ocupantes del vehículo asegurado hasta los límites indicados en las Condiciones Particulares cuando hayan sido contratadas, como consecuencia de un accidente ocasionado por el vehículo asegurado descrito en la póliza. La cobertura de Accidentes de Ocupantes solo se activará cuando el vehículo asegurado participe en un accidente durante su circulación y siempre y cuando el siniestro se encuentre cubierto por la póliza.",
+        description: "Responderá por las coberturas de muerte, invalidez permanente total o parcial y gastos de curación de los ocupantes de su vehículo hasta los límites indicados en las Condiciones Particulares cuando hayan sido contratadas, como consecuencia de un accidente ocasionado por su vehículo.",
         solutions:[1,4,5],
         id: 5
     },
     {
         nameDamage:"Daño a terceros",
         damage:"Accidente que involucre a terceros",
-        description:"Cubre la responsabilidad civil extracontractual en que incurra el Asegurado o cualquier otra persona que durante la circulación del vehículo asegurado con autorización de este último, ocasiones lesiones y/o daños materiales a personas o cosas que se encuentren fuera de él como consecuencia de un accidente o serie de accidentes emanados en un solo suceso o acontecimiento.",
+        description:"Cubre la responsabilidad civil extracontractual en que incurra usted o cualquier otra persona que durante la circulación de su vehículo asegurado con autorización de este último, ocasiones lesiones y/o daños materiales a personas o cosas que se encuentren fuera de él como consecuencia de un accidente o serie de accidentes emanados en un solo suceso o acontecimiento.",
         solutions:[0,1,4],
         id:6
     }
