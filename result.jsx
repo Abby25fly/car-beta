b87d45b3bd4a085a96800783c00c848242e3f5c8
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-17 10:36:09 -0500

    componentes

--- result.jsx
+++ result.jsx
@@ -14,8 +14,7 @@ class FinalData extends Component {
         phone: null
       }
     }
-  
-  
+    
     Solutions.map(item => {
       return (<div className="container">
       <Col xs={6} md={4} id="card">
@@ -32,53 +31,53 @@ class FinalData extends Component {
   }
 
   
-const DataProcessEnough = () => (
-  <div className="container">
-    <Col xs={6} md={4} id="card">
-      <Thumbnail src="../public/avatar.png" alt="242x200">
-        <h3 id="name">Name's client</h3>
-        <p id="info-card">Description</p>
-      </Thumbnail>
-      <div>
-        <Button id="btnNext"><i><Glyphicon glyph="earphone" /></i></Button>
-      </div>
-    </Col>
-  </div>);
+// const DataProcessEnough = () => (
+//   <div className="container">
+//     <Col xs={6} md={4} id="card">
+//       <Thumbnail src="../public/avatar.png" alt="242x200">
+//         <h3 id="name">Name's client</h3>
+//         <p id="info-card">Description</p>
+//       </Thumbnail>
+//       <div>
+//         <Button id="btnNext"><i><Glyphicon glyph="earphone" /></i></Button>
+//       </div>
+//     </Col>
+//   </div>);
 
-const DataProcessCorrect = () => (
-  <div className="container">
-    <Col xs={6} md={4} id="card">
-      <Thumbnail src="../public/avatar.png">
-        <h3 id="name">Name's client</h3>
-        <p id="info-card">Description</p>
+// const DataProcessCorrect = () => (
+//   <div className="container">
+//     <Col xs={6} md={4} id="card">
+//       <Thumbnail src="../public/avatar.png">
+//         <h3 id="name">Name's client</h3>
+//         <p id="info-card">Description</p>
 
-        <div className="well">
+//         <div className="well">
 
-          <Button id="btnNext" bsSize="large" block>Confirmar</Button>
-        </div>
+//           <Button id="btnNext" bsSize="large" block>Confirmar</Button>
+//         </div>
 
-      </Thumbnail>
-    </Col>
-  </div>
-);
+//       </Thumbnail>
+//     </Col>
+//   </div>
+// );
 
 
 
-const BtnCall = () => (
-  <div className="well" id="btnNext">
+// const BtnCall = () => (
+//   <div className="well" id="btnNext">
 
-    <Button bsSize="large" block>Llamar</Button>
-  </div>
+//     <Button bsSize="large" block>Llamar</Button>
+//   </div>
 
-);
+// );
 
-const BtnNext = () => (
-  <div className="well" id="btnNext">
+// const BtnNext = () => (
+//   <div className="well" id="btnNext">
 
-    <Button bsSize="large" block>Siguiente</Button>
-  </div>
+//     <Button bsSize="large" block>Siguiente</Button>
+//   </div>
 
-);
+// );
 
 }  
 export default FinalData;
