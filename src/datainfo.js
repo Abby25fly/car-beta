9f8a98508a873182fb7cb8768bcd19e9f327fe4c
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-18 07:14:11 -0500

    final

--- src/datainfo.js
+++ src/datainfo.js
@@ -4,48 +4,48 @@ const DataInfo ={
     {
         nameDamage:"Desbarranco",
         damage:"Daño total del vehículo",
-        description:"Su seguro le devuelve un nuevo vehículo con las características dadas al momento de la adquisición del seguro.",
-        solutions:[0,1,3,4,5],
+        description:"Devolverle un nuevo vehículo con las características dadas al momento de la adquisición del seguro.",
+        solutions:[0,1,3,5],
         id:1
     },
     {
         nameDamage: "Avería Mecánica",
         damage:"Falla mecánica",
-        description: "Su seguro le garantiza la reparación total del auto, enviandole un mecánico a su ubicación y/o grúa de ser necesario.",
+        description: "Garantizarle la reparación total de su auto, enviandole un mecánico a su ubicación y/o grúa de ser necesario.",
         solutions:[0,1,3],
         id: 2
     },
     {
         nameDamage: "Choque provocado por el conductor",
         damage:"Daño Propio",
-        description: "Cubre los daños o pérdidas que sufra el vehículo asegurado por las maniobras hechas por el conductor: rotura de lunas y accesorios musicales.",
-        solutions:[0,1,3,5],
+        description: "Cubrir los daños o pérdidas que sufra su vehículo por las maniobras hechas por el conductor: rotura de lunas y accesorios musicales.",
+        solutions:[0,1,3,4],
         id: 3
     },
     {
         nameDamage: "Choque Frontal",
         damage:"Accidente de los ocupantes",
-        description: "Su seguro responderá por las coberturas de gastos de curación de los ocupantes del vehículo asegurado y de los daños ocasionados al vehiculo.",
-        solutions:[0,4,5],
+        description: "Cubrir de gastos de curación de los ocupantes de su vehículo y de los daños ocasionados a su vehículo.",
+        solutions:[0,4],
         id: 4
     },
     {
         nameDamage: "Volcadura",
         damage:"Daño por despite o imperfecto mecánico",
-        description: "Su seguro responderá por las coberturas de muerte, invalidez permanente total o parcial y gastos de curación de los ocupantes del vehículo asegurado hasta los límites indicados en las Condiciones Particulares cuando hayan sido contratadas, como consecuencia de un accidente ocasionado por el vehículo asegurado descrito en la póliza. La cobertura de Accidentes de Ocupantes solo se activará cuando el vehículo asegurado participe en un accidente durante su circulación y siempre y cuando el siniestro se encuentre cubierto por la póliza.",
-        solutions:[1,4,5],
+        description: "Cubrir los gatos de muerte, invalidez permanente total o parcial y gastos de curación de los ocupantes de su vehículo.",
+        solutions:[2,3,4,5],
         id: 5
     },
     {
-        nameDamage:"Daño a terceros",
-        damage:"Accidente que involucre a terceros",
-        description:"Cubre la responsabilidad civil extracontractual en que incurra el Asegurado o cualquier otra persona que durante la circulación del vehículo asegurado con autorización de este último, ocasiones lesiones y/o daños materiales a personas o cosas que se encuentren fuera de él como consecuencia de un accidente o serie de accidentes emanados en un solo suceso o acontecimiento.",
-        solutions:[0,1,4],
+        nameDamage:"Robo de Vehículo",
+        damage:"Pérdida Total del Vehículo",
+        description:"Pagará el Valor Comercial de su vehículo en el mercado peruano en la fecha del siniestro.",
+        solutions:[3],
         id:6
     }
 
 
 ],
-    Solutions: ["Servicio Mecánico", "Servicio de Grúa","Chofer de reemplazo", "Carro de reemplazo","Asistente de la Positiva Seguros al lugar del incidente","Ambulancia"]
+    Solutions: ["Servicio Mecánico", "Servicio de Grúa","Chofer de reemplazo", "Carro de reemplazo","Ambulancia"]
 }
 export default DataInfo;
